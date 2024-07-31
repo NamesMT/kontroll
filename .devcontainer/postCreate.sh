5d76074591b30447caeda3f0a23e2bebd5307476
NamesMT <dangquoctrung123@gmail.com>
2024-07-31 14:06:18 +0000

    chore: remove old `.devcontainer`

--- .devcontainer/postCreate.sh
+++
@@ -1,2 +0,0 @@
-# Force pnpm to use global store instead of $PWD, ref: https://stackoverflow.com/questions/76573556/pnpm-use-docker-volume-as-store-path
-pnpm config -g set store-dir $PNPM_HOME/store
